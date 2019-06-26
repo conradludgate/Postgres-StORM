9265cfad7630c858933296b0af92f8676817ce50
Conrad Ludgate <oon@conradludgate.com>
2019-06-26 18:30:48 +0100

    No longer need quote marks around everything. Handled by me in use case

--- Sources/PostgresStORM/Select.swift
+++ Sources/PostgresStORM/Select.swift
@@ -104,13 +104,13 @@ extension PostgresStORM {
 		var clauseOrder = ""
 
 		if columns.count > 0 {
-			clauseSelectList = "\""+columns.joined(separator: "\",\"")+"\""
+			clauseSelectList = columns.joined(separator: ",")
 		} else {
 			var keys = [String]()
 			for i in cols() {
 				keys.append(i.0)
 			}
-			clauseSelectList = "\""+keys.joined(separator: "\",\"")+"\""
+			clauseSelectList = keys.joined(separator: ",")
 		}
 		if whereclause.count > 0 {
 			clauseWhere = " WHERE \(whereclause)"
